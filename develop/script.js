05207998e525c465c40b612fb85583937fe39e1a
Tripp's Repo <tripp.parham@me.com>
2019-12-17 01:36:01 -0500

    Major Progress Update

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,6 @@
-
 $(document).ready(function () {
     writeToSchedule();
-    //localStorage.clear();
+    // localStorage.clear();
     cities = JSON.parse(localStorage.getItem("cities")) || [];
     $('#submitWeather').click(function (event) {
         // event.preventDefault() prevents the form from trying to submit itself.
@@ -11,23 +10,23 @@ $(document).ready(function () {
         var newCity = $("#locale").val().trim();
 
 
-        //Create array to store high scores
+        // Create array to store high scores
         cities = JSON.parse(localStorage.getItem("cities")) || [];
-        
+
         console.log(cities);
 
-        //add new city to cities
-        if(newCity != ""){
-        cities.unshift(newCity);
-        cities = Array.from(new Set(cities));
-        if(cities.length >= 10){
-            cities.pop();
-        }
-        //cities.reverse();
-}// Remove any appointments previosly scheduled at the same time as new appointment.
-       
+        // add new city to cities
+        if (newCity != "") {
+            cities.unshift(newCity);
+            cities = Array.from(new Set(cities));
+            if (cities.length >= 10) {
+                cities.pop();
+            }
+            // cities.reverse();
+        } // Remove any appointments previosly scheduled at the same time as new appointment.
+
         console.log(cities);
-        //Update local storage with revised cities
+        // Update local storage with revised cities
         localStorage.setItem('cities', JSON.stringify(cities));
         console.log(cities);
         // The city from the textbox is then added to our array
@@ -35,58 +34,28 @@ $(document).ready(function () {
         // calling renderButtons which handles the processing of our city array
         renderButtons();
         console.log(city);
-        if (city != '') {
-            $("#error").html("");
+        getTheWeather(city);
 
-            $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=05704eed827c348a42aefa846e03d80c",
-                // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
-                method: "GET"
-            })
-                .then(function (response) {
-
-                    console.log(response);
-                 
-                    postToHtml(response);
-                    $.ajax({
-                        url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&APPID=05704eed827c348a42aefa846e03d80c",
-                        method: "GET"
-                    })
-                        .then(function (data) {
-
-                            console.log(data);
-                            postUV(data);
-                        });
-                });
-
-
-        } else {
-            $("#error").html('Field cannot be empty');
-        }
-    })
+    });
+    $(document).on("click", ".fatCity", function () {
+        console.log("fatCity button pushed");
+        var city = $(this).attr("data-name");
+        getTheWeather(city);
+    });
+    // $(document).on("click", "#clearStorage", localStorage.clear());
+   // $('#clearStorage').click(function (event) {
+    //    clearStorage();
+    //});
+});
+$('#clearStorage').click(function (event) {
+    clearStorage();
 });
 
-
-
-//var APIKey = "05704eed827c348a42aefa846e03d80c";
+// var APIKey = "05704eed827c348a42aefa846e03d80c";
 
 // Here we are building the URL we need to query the database
 // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-//  "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
-
-// Here we run our AJAX call to the OpenWeatherMap API
-//$.ajax({
-//   url: queryURL,
-//  method: "GET"
-// // })
-// We store all of the retrieved data inside of an object called "response"
-// .then(function(response) {
-
-// Log the queryURL
-// console.log(queryURL);
-
-// Log the resulting object
-// console.log(response);
+// "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
 
 
 var cities = [];
@@ -96,37 +65,51 @@ var dateTwo = moment(startDate, 'dddd').add(48, 'hours');
 var dateThree = moment(startDate, 'dddd').add(72, 'hours');
 var dateFour = moment(startDate, 'dddd').add(96, 'hours');
 var dateFive = moment(startDate, 'dddd').add(120, 'hours');
-$("#timeOne").html("<h4>" + dateOne.format('dddd') + "<h4>");
-$("#timeTwo").html("<h4>" + dateTwo.format('dddd') + "<h4>");
-$("#timeThree").html("<h4>" + dateThree.format('dddd') + "<h4>");
-$("#timeFour").html("<h4>" + dateFour.format('dddd') + "<h4>");
-$("#timeFive").html("<h4>" + dateFive.format('dddd') + "<h4>");
+/*$("#time1").html("<h4>" + dateOne.format('dddd') + "<h4>");
+$("#time2").html("<h4>" + dateTwo.format('dddd') + "<h4>");
+$("#time3").html("<h4>" + dateThree.format('dddd') + "<h4>");
+$("#time4").html("<h4>" + dateFour.format('dddd') + "<h4>");
+$("#time5").html("<h4>" + dateFive.format('dddd') + "<h4>");*/
 // Transfer content to HTML
-function postToHtml(response) {
-    // Converts the temp to Kelvin with the below formula
+function postToHtml(response) { // Converts the temp to Kelvin with the below formula
     var tempF = (response.main.temp - 273.15) * 1.80 + 32;
     $(".tempF").text("Temperature" + tempF + "Fahrenheit");
-    var iconcode = response.weather.icon;
-    var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(iconurl);
+    // var iconcode = response.weather.icon;
+    // var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+    // console.log(iconurl);
     $(".city").html("<h1>" + response.name + " (" + startDate.format('L') + ") </h1>");
-    $("#wicon").attr('src', iconurl);
+    // $("#wicon").attr('src', iconurl);
     $(".wind").text("Wind Speed: " + response.wind.speed.toFixed(0) + " mph");
     $(".humidity").text("Humidity: " + response.main.humidity + "%");
     $(".temp").text("Temperature: " + tempF.toFixed(0) + "°F");
-
-
 
     // Log the data in the console as well
     console.log("Wind Speed: " + response.wind.speed);
     console.log("Humidity: " + response.main.humidity);
     console.log("Temperature (F): " + tempF);
+
 };
-// Transfer content to HTML
+// Transfer five day forcast content to HTML
+function fiveDayForcast(response) {
+    for (i = 0; i < 5; i ++) {
+        if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+            var tempF = list[i].main.temp;
+            $("#dayContainer").html("<div class='column' id='dayColumn'><div id='forcastBox'><p id='time'" + i + 1 + "></p> <div class='weatherIcon'></div><div class='temp'></div><div class='humidity'></div></div>'")
+            $(".weatherIcon").append(response.list[i].weather.icon);
+            $(".humidity").text("Humidity: " + response.list[i].main.humidity + "%");
+            $(".temp").text("Temperature: " + tempF.toFixed(0) + "°F");
+        } // End if
+    } // End For loop
+    $("#time1").html("<h4>" + dateOne.format('dddd') + "<h4>");
+    $("#time2").html("<h4>" + dateTwo.format('dddd') + "<h4>");
+    $("#time3").html("<h4>" + dateThree.format('dddd') + "<h4>");
+    $("#time4").html("<h4>" + dateFour.format('dddd') + "<h4>");
+    $("#time5").html("<h4>" + dateFive.format('dddd') + "<h4>");
+}
+// End Function
 
 // Function for displaying city history data
 function renderButtons() {
-
     // Deleting the city buttons prior to adding new city buttons
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
@@ -136,7 +119,7 @@ function renderButtons() {
 
         // Then dynamicaly generating buttons for each movie in the array.
         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
+        var a = $("<button id='cityButton'>");
         // Adding a class
         a.addClass("fatCity");
         // Adding a data-attribute with a value of the movie at index i
@@ -158,25 +141,64 @@ function postUV(data) {
     }
 }
 
+
 // Write the schedule array to html
 function writeToSchedule() {
     cities = JSON.parse(localStorage.getItem("cities")) || [];
     // Deleting the city buttons prior to adding new city buttons
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
-        // Looping through the array of movies
-        for (var i = 0; i < cities.length; i++) {
+    // Looping through the array of movies
+    for (var i = 0; i < cities.length; i++) {
 
-            // Then dynamicaly generating buttons for each movie in the array.
-            // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-            var a = $("<button>");
-            // Adding a class
-            a.addClass("fatCity");
-            // Adding a data-attribute with a value of the movie at index i
-            a.attr("data-name", cities[i]);
-            // Providing the button's text with a value of the movie at index i
-            a.text(cities[i]);
-            // Adding the button to the HTML
-            $("#buttons-view").append(a);
-        }
+        // Then dynamicaly generating buttons for each movie in the array.
+        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class
+        a.addClass("fatCity");
+        // Adding a data-attribute with a value of the movie at index i
+        a.attr("data-name", cities[i]);
+        // Providing the button's text with a value of the movie at index i
+        a.text(cities[i]);
+        // Adding the button to the HTML
+        $("#buttons-view").append(a);
+    }
+
+}
+function getTheWeather(city) {
+    console.log("getTheWeather function was called")
+    if (city != '') {
+        $("#error").html(""); // Clears error message field when text is entered
+
+        $.ajax({ // AJAX call for current conditions
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=05704eed827c348a42aefa846e03d80c",
+            method: "GET"
+        }).then(function (response) {
+
+            console.log(response);
+            postToHtml(response);
+            $.ajax({ // AJAX call for UV data
+                url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&APPID=05704eed827c348a42aefa846e03d80c",
+                method: "GET"
+            }).then(function (data) { // Post UV to HTML.then function data
+                console.log(data);
+                postUV(data);
+
+            });
+        });
+        $.ajax({ // AJAX call for 5 day forcast ex...api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
+            url: "https://api.openweathermap.org/data/2.5/forcast?q=" + city + ",us&APPID=05704eed827c348a42aefa846e03d80c",
+            method: "GET"
+        }).then(function (response) { // Post current forcast to HTML.then function response
+            console.log(response);
+            fiveDayForcast(response);
+        });
+    } else { // Error message if user doesn't enter anything
+        $("#error").html('Field cannot be empty');
+    }
+}
+function clearStorage() {
+    console.log("Local storage to be cleared");
+    localStorage.clear();
+    $("#buttons-view").empty();
 }
